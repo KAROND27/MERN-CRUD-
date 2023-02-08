89913bbcbcc89e71d5ec987083421a404ab7535a
KAROND <alisadiq1314@gmail.com>
2023-02-08 08:49:05 -0500

    main

--- server/app.js
+++ server/app.js
@@ -1,17 +1,16 @@
-const express = require('express')
+const express = require('express');
 require('dotenv').config();
-const app = express()
-const port = 5000
-const cors=require("cors");
-app.use(cors({origin: true}))
+const app = express();
+const port = 5000;
+const cors = require('cors');
+app.use(cors({ origin: true }));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 require('./db/connect');
-const Data=require("./models/Schema")
-const roote=require("../server/routes/route")
+const Data = require('./models/Schema');
+const roote = require('../server/routes/route');
 app.use(roote);
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
